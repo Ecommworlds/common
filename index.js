3bf9a9e5e1307aabbceb8c3dad42eeb89c32b0e1
Ecommworlds <106814106+Ecommworlds@users.noreply.github.com>
2022-07-14 21:20:20 +0530

    commit-2

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 "use strict";
-exports.__esModule = true;
-exports.myVar = "Hello";
+const tslib_1 = require("tslib");
+const express_1 = tslib_1.__importDefault(require("express"));
+const cors_1 = tslib_1.__importDefault(require("cors"));
+const http_errors_1 = tslib_1.__importDefault(require("http-errors"));
+express_1.default.response.sendHttpSuccess = function (results, message = "OK", statusCode = 200) {
+    return this.contentType('application/json').status(statusCode).send({ message, code: statusCode, error: false, results });
+};
+express_1.default.response.sendHttpError = function (statusCode, message, error = {}, properties = {}) {
+    const errorDetails = { name: error.name, message: error.message, stack: error.stack };
+    return this.contentType('application/json').status(statusCode).send({ message, code: statusCode, error: true, errorMessage: (0, http_errors_1.default)(statusCode).message, errorDetails, properties });
+};
+express_1.default.response.sendValidationError = function (errors, message = "Validation Errors", statusCode = 422) {
+    return this.contentType('application/json').status(statusCode).send({ message, code: statusCode, error: true, errorMessage: (0, http_errors_1.default)(statusCode).message, errors });
+};
+const app = (0, express_1.default)();
+app.enable("trust proxy");
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+module.exports = app;
